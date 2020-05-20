5d3c33ec70547f223e2b5bbcb6460475d40dd789
Misko <Misko>
2020-05-20 20:03:14 +0200

    Podesena osnova mikroservisa za izmenu oglasa.

---
+++ IzmenaOglasa/src/main/java/com/example/demo/model/Ad.java
@@ -0,0 +1,76 @@
+package com.example.demo.model;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ad {
+
+	private Renter renter;
+	private Vehicle vehicle;
+	private Calendar date;
+	private Calendar validTru;
+	private Calendar validFrom;
+	private Map<Calendar, Boolean> free = new HashMap<Calendar, Boolean>();
+	private int mileage;
+
+	public Ad() {
+
+	}
+
+	public Ad(Renter renter, Vehicle vehicle, Calendar date, Calendar validTru, Calendar validFrom,
+			HashMap<Calendar, Boolean> free, int mileage) {
+		super();
+		this.renter = renter;
+		this.vehicle = vehicle;
+		this.date = date;
+		this.validTru = validTru;
+		this.validFrom = validFrom;
+		this.free = free;
+		this.mileage = mileage;
+	}
+	public Renter getUser() {
+		return renter;
+	}
+	public void setUser(Renter renter) {
+		this.renter = renter;
+	}
+	public Vehicle getVehicle() {
+		return vehicle;
+	}
+	public void setVehicle(Vehicle vehicle) {
+		this.vehicle = vehicle;
+	}
+	public Calendar getDate() {
+		return date;
+	}
+	public void setDate(Calendar date) {
+		this.date = date;
+	}
+	public Calendar getValidTru() {
+		return validTru;
+	}
+	public void setValidTru(Calendar validTru) {
+		this.validTru = validTru;
+	}
+	public Calendar getValidFrom() {
+		return validFrom;
+	}
+	public void setValidFrom(Calendar validFrom) {
+		this.validFrom = validFrom;
+	}
+	public Map<Calendar, Boolean> getFree() {
+		return free;
+	}
+	public void setFree(HashMap<Calendar, Boolean> free) {
+		this.free = free;
+	}
+	public int getMileage() {
+		return mileage;
+	}
+	public void setMileage(int mileage) {
+		this.mileage = mileage;
+	}
+
+
+}
---
+++ IzmenaOglasa/src/main/java/com/example/demo/model/Message.java
@@ -0,0 +1,67 @@
+package com.example.demo.model;
+
+import java.util.Calendar;
+
+public class Message {
+
+	private UserModel sender;
+	private UserModel reciever;
+	private String subject;
+	private String content;
+	private Calendar date;
+
+	public Message() {
+
+	}
+
+	public Message(UserModel sender, UserModel reciever, String subject, String content, Calendar date) {
+		super();
+		this.sender = sender;
+		this.reciever = reciever;
+		this.subject = subject;
+		this.content = content;
+		this.date = date;
+	}
+
+	public UserModel getSender() {
+		return sender;
+	}
+
+	public void setSender(UserModel sender) {
+		this.sender = sender;
+	}
+
+	public UserModel getReciever() {
+		return reciever;
+	}
+
+	public void setReciever(UserModel reciever) {
+		this.reciever = reciever;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Calendar getDate() {
+		return date;
+	}
+
+	public void setDate(Calendar date) {
+		this.date = date;
+	}
+
+
+}
