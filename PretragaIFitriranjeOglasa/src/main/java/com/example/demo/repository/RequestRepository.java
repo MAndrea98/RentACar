9e1039eba3e0caa7e010d2dc0f1c04ec5175c452
miskosekula <sekulicmilorad7@gmail.com>
2020-06-05 16:45:13 +0200

    Izmena

---
+++ PretragaIFitriranjeOglasa/src/main/java/com/example/demo/repository/RequestRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.model.Request;
+
+public interface RequestRepository extends JpaRepository<Request, Long> {
+
+
+
+}
