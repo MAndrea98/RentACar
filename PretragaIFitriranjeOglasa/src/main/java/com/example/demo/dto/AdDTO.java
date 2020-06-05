8848b1a83c42c43f36877f46bef8831d29272c82
miskosekula <sekulicmilorad7@gmail.com>
2020-06-05 13:45:08 +0200

    Dodavanje u mikroservis za pretragu

--- PretragaIFitriranjeOglasa/src/main/java/com/example/demo/dto/AdDTO.java
+++ PretragaIFitriranjeOglasa/src/main/java/com/example/demo/dto/AdDTO.java
@@ -1,42 +1,35 @@
-package com.example.demo.model;
+package com.example.demo.dto;
 
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.example.demo.model.EndUser;
+import com.example.demo.model.Renter;
+import com.example.demo.model.Vehicle;
 
-public class Ad {
+
+public class AdDTO {
 
 
 	private Long id;
-
 	private Renter renter;
-
 	private EndUser endUser;
-
 	private Vehicle vehicle;
-
 	private String place;
-
 	private Calendar date;
-
 	private Calendar validTru;
-
 	private Calendar validFrom;
-
 	private Calendar dateFrom;
-
 	private Calendar dateTo;
-
 	private Map<Calendar, Boolean> free = new HashMap<Calendar, Boolean>();
-
 	private int mileage;
 
-	public Ad() {
+	public AdDTO() {
 
 	}
 
-	public Ad(Renter renter, Vehicle vehicle, String place, Calendar date, Calendar validTru, Calendar validFrom,
+	public AdDTO(Renter renter, Vehicle vehicle, String place, Calendar date, Calendar validTru, Calendar validFrom,
 			Calendar dateFrom, Calendar dateTo, HashMap<Calendar, Boolean> free, int mileage) {
 		super();
 		this.renter = renter;
@@ -50,30 +43,6 @@ public class Ad {
 		this.free = free;
 		this.mileage = mileage;
 	}
-	public String getPlace() {
-		return place;
-	}
-
-	public void setPlace(String place) {
-		this.place = place;
-	}
-
-	public Calendar getDateFrom() {
-		return dateFrom;
-	}
-
-	public void setDateFrom(Calendar dateFrom) {
-		this.dateFrom = dateFrom;
-	}
-
-	public Calendar getDateTo() {
-		return dateTo;
-	}
-
-	public void setDateTo(Calendar dateTo) {
-		this.dateTo = dateTo;
-	}
-
 	public Renter getUser() {
 		return renter;
 	}
@@ -115,6 +84,29 @@ public class Ad {
 	}
 	public void setMileage(int mileage) {
 		this.mileage = mileage;
+	}
+	public String getPlace() {
+		return place;
+	}
+
+	public void setPlace(String place) {
+		this.place = place;
+	}
+
+	public Calendar getDateFrom() {
+		return dateFrom;
+	}
+
+	public void setDateFrom(Calendar dateFrom) {
+		this.dateFrom = dateFrom;
+	}
+
+	public Calendar getDateTo() {
+		return dateTo;
+	}
+
+	public void setDateTo(Calendar dateTo) {
+		this.dateTo = dateTo;
 	}
 
 
