a2ba69647f466f7bb9372f09d6688284f4087b47
MilenaVujicic <44505736+MilenaVujicic@users.noreply.github.com>
2020-06-17 20:40:37 +0200

    Added navigation bars

--- RentACarFrontend/src/app/app.module.ts
+++ RentACarFrontend/src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbDropdown} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DefaultNavComponent } from './default-nav/default-nav.component';
+import { AdminNavComponent } from './admin-nav/admin-nav.component';
+import { UserNavComponent } from './user-nav/user-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DefaultNavComponent
+    DefaultNavComponent,
+    AdminNavComponent,
+    UserNavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
