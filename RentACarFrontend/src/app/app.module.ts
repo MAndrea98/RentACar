6d573d38c133477212725c788212fd74672f2b67
MilenaVujicic <44505736+MilenaVujicic@users.noreply.github.com>
2020-06-17 19:10:52 +0200

    Added navigation component

--- RentACarFrontend/src/app/app.module.ts
+++ RentACarFrontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DefaultNavComponent } from './default-nav/default-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultNavComponent
   ],
   imports: [
     BrowserModule,
