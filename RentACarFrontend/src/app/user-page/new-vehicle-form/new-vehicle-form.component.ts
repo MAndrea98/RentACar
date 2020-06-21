85f0fb378e81f0e09ce6d2ac2784f13adc85835b
MilenaVujicic <44505736+MilenaVujicic@users.noreply.github.com>
2020-06-21 16:54:26 +0200

    Added components

---
+++ RentACarFrontend/src/app/user-page/new-vehicle-form/new-vehicle-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-vehicle-form',
+  templateUrl: './new-vehicle-form.component.html',
+  styleUrls: ['./new-vehicle-form.component.css']
+})
+export class NewVehicleFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
