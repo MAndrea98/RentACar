e2fb6857389d90e17b36fed0d71b0f0bb5d67b12
Postin <filip.vozarevic@gmail.com>
2020-06-21 12:45:34 +0200

    Konfiguracija za SOAP zakomentarisana u agentskoj

---
+++ RentACar/src/main/java/com/example/demo/client/RazmenaPorukaClient.java
@@ -0,0 +1,10 @@
+package com.example.demo.client;
+
+import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RazmenaPorukaClient extends WebServiceGatewaySupport  {
+	private static final Logger log = LoggerFactory.getLogger(RazmenaPorukaClient.class);
+	
+}
