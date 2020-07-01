e8ee3cbf69a81be7a889652e79b8ccc2bb59925a
Postin <filip.vozarevic@gmail.com>
2020-07-01 15:07:25 +0200

    Odradjen client za SOAP u agentskoj za razmenu poruka, ocenjivanje i komentare, i administratora

--- RentACar/src/main/java/com/example/demo/client/OcenjivanjeIKomentariClient.java
+++ RentACar/src/main/java/com/example/demo/client/OcenjivanjeIKomentariClient.java
@@ -1,5 +1,57 @@
 package com.example.demo.client;
 
-public class OcenjivanjeIKomentariClient {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+import org.springframework.ws.soap.client.core.SoapActionCallback;
+
+import com.example.consumingwebservice.CreateReviewRequest;
+import com.example.consumingwebservice.CreateReviewResponse;
+import com.example.consumingwebservice.DeleteReviewRequest;
+import com.example.consumingwebservice.DeleteReviewResponse;
+import com.example.consumingwebservice.EditReviewRequest;
+import com.example.consumingwebservice.EditReviewResponse;
+import com.example.consumingwebservice.ReviewDTOOIK;
+
+public class OcenjivanjeIKomentariClient extends WebServiceGatewaySupport {
+	private static final Logger log = LoggerFactory.getLogger(RazmenaPorukaClient.class);
+	
+	public CreateReviewResponse createReview(ReviewDTOOIK review) {
+		CreateReviewRequest request= new CreateReviewRequest();
+		request.setReview(review);
+		
+		log.info("Requesting create review status ");
+		
+		CreateReviewResponse response  = (CreateReviewResponse) getWebServiceTemplate()
+				 .marshalSendAndReceive("http://localhost:8085/ws/reviews", request,
+						 new SoapActionCallback("http://localhost:8085/api/OcenjivanjeIKomentari/createReviewRequest"));
+		
+		return response;
+	}
+	
+	public EditReviewResponse editReview(ReviewDTOOIK review) {
+		EditReviewRequest request = new EditReviewRequest();
+		
+		log.info("Requesting create review status ");
+		
+		EditReviewResponse response = (EditReviewResponse) getWebServiceTemplate()
+				 .marshalSendAndReceive("http://localhost:8085/ws/reviews", request,
+						 new SoapActionCallback("http://localhost:8085/api/OcenjivanjeIKomentari/editReviewRequest"));
+		
+		return response;
+	}
+	
+	public DeleteReviewResponse deleteReview(Long id) {
+		DeleteReviewRequest request = new DeleteReviewRequest();
+		
+		log.info("Requesting create review status ");
+		
+		DeleteReviewResponse response = (DeleteReviewResponse) getWebServiceTemplate()
+				 .marshalSendAndReceive("http://localhost:8085/ws/reviews", request,
+						 new SoapActionCallback("http://localhost:8085/api/OcenjivanjeIKomentari/deleteReviewRequest"));
+		
+		return response;
+	}
+	
 
 }
--- RentACar/src/main/java/com/example/demo/client/RazmenaPorukaClient.java
+++ RentACar/src/main/java/com/example/demo/client/RazmenaPorukaClient.java
@@ -3,6 +3,11 @@ package com.example.demo.client;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 import org.springframework.ws.soap.client.core.SoapActionCallback;
 
+import com.example.consumingwebservice.DeleteMessageRequest;
+import com.example.consumingwebservice.DeleteMessageResponse;
+import com.example.consumingwebservice.EditMessageRequest;
+import com.example.consumingwebservice.EditMessageResponse;
+import com.example.consumingwebservice.GetAllMessageResponse;
 import com.example.consumingwebservice.MessageRP;
 import com.example.consumingwebservice.SendMessageRequest;
 import com.example.consumingwebservice.SendMessageResponse;
@@ -17,7 +22,7 @@ public class RazmenaPorukaClient extends WebServiceGatewaySupport  {
 		SendMessageRequest request = new SendMessageRequest();
 		request.setMessage(m);
 				
-		 log.info("Requesting message status ");
+		 log.info("Requesting send message status ");
 		 
 		 SendMessageResponse response  = (SendMessageResponse) getWebServiceTemplate()
 				 .marshalSendAndReceive("http://localhost:8089/ws/messages", request,
@@ -25,5 +30,43 @@ public class RazmenaPorukaClient extends WebServiceGatewaySupport  {
 		 
 		 return response;
 	}
+	
+	public EditMessageResponse editMessage(MessageRP m) {
+		EditMessageRequest request = new EditMessageRequest();
+		request.setMessage(m);
+		
+		log.info("Requesting edit message status ");
+		
+		 EditMessageResponse response  = (EditMessageResponse) getWebServiceTemplate()
+				 .marshalSendAndReceive("http://localhost:8089/ws/messages", request,
+						 new SoapActionCallback("http://localhost:8089/api/RazmenaPoruka/editMessageRequest"));
+		 
+		 return response;
+	}
+	
+	public DeleteMessageResponse deleteMessage(Long id) {
+		DeleteMessageRequest request = new DeleteMessageRequest();
+		request.setMessageId(id);
+		
+		log.info("Requesting delete message status ");
+		
+		DeleteMessageResponse response  = (DeleteMessageResponse) getWebServiceTemplate()
+				 .marshalSendAndReceive("http://localhost:8089/ws/messages", request,
+						 new SoapActionCallback("http://localhost:8089/api/RazmenaPoruka/deleteMessageRequest"));
+		 
+		 return response;
+		
+	}
+	
+	public GetAllMessageResponse getAllMessages() {		
+		log.info("Requesting all messages ");
+		
+		GetAllMessageResponse response = (GetAllMessageResponse) getWebServiceTemplate()
+				.marshalSendAndReceive("http://localhost:8089/ws/messages", null,
+						 new SoapActionCallback("http://localhost:8089/api/RazmenaPoruka/deleteMessageRequest"));
+		
+		return response;
+	}
+	
 	
 }
