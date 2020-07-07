8e4df6fc53a38597c22679adc5f6103f38a361d0
MAndrea98 <mendrei.andrea@gmail.com>
2020-07-07 13:27:38 +0200

    Uradjen frontend za poruke

---
+++ RentACarFrontend/src/app/user-page/user-messages/user-messages.component.ts
@@ -0,0 +1,15 @@
+import { OnInit, Component } from '@angular/core';
+
+@Component({
+    selector: 'app-user-messages',
+    templateUrl: './user-messages.component.html',
+    styleUrls: ['./user-messages.component.css']
+})
+
+export class UserMessagesComponent implements OnInit {
+    
+    ngOnInit(): void {
+        throw new Error("Method not implemented.");
+    }
+    
+}
